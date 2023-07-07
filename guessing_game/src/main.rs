f8e90798241f51b4a169af76d25b7e25590c6092
陈湘 <chenxiangbj@yuanfudao.com>
2023-07-07 17:44:14 +0800

    add: 猜数字游戏

---
+++ guessing_game/src/main.rs
@@ -0,0 +1,28 @@
+use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
+fn main() {
+    println!("猜一个数字");
+    let select_number = rand::thread_rng().gen_range(1..=100);
+    loop {
+        println!("请输入数字");
+        let mut guess = String::new();
+        io::stdin().read_line(&mut guess).expect("读取行失败");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+        println!("你猜的数字是: {guess}");
+        match guess.cmp(&select_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+    }
+}
+
+
+// match ... {} 这块的处理挺有意思，好像 js 的 switch+catch，很方便
